70193262b4ff0c4b31952eb1a72669a9268f43cb
HARSH TIBREWAL <146108437+harshtibrewal02@users.noreply.github.com>
2024-04-16 15:24:57 +0000

    dataTypes in JS

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // it treates all JS code as a newer version
+
+// alert(3+3)  , we are using using nodejs , not browser
+
+console.log("HARSH")
+
+// code readability should be high
+
+console.log(3+3)
+
+let name= " Harsh"
+let age=21
+let isLoggedin = true // it is boolean data type , can be true or false
+let state; // undefined 
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true or false 
+// null => standalone value , is a typeof object
+// undefined =>  
+// symbol => unique
+
+
+// object
+
+console.log(typeof null);
