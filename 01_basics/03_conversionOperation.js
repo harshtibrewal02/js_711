38b4f57d6dbc5ff6e3090ea3d00b344390d6997d
HARSH TIBREWAL <146108437+harshtibrewal02@users.noreply.github.com>
2024-04-17 15:44:44 +0000

    Conversion of dataTypes in JS

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,55 @@
+// let score="51"
+
+// console.log(typeof score);
+// console.log(typeof (score)); // second way of printing any data type
+
+
+// let valueInNumber = Number(score) // now score has been converted in number data type
+// console.log(typeof valueInNumber);
+
+// let age ="33abc"
+
+// let valueInNumber2 = Number(age)
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2);
+
+
+// let score2 = null
+// let valueInNumber3 = Number(score2)
+// console.log(typeof valueInNumber3);
+// console.log(valueInNumber3);
+
+
+// let score3 = undefined
+// let valueInNumber4 = Number(score3)
+// console.log(typeof valueInNumber4);
+// console.log(valueInNumber4);
+
+
+// let score4 = true
+// let valueInNumber5 = Number(score4)
+// console.log(typeof valueInNumber5);
+// console.log(valueInNumber5);
+
+
+// "51" => 51
+// "51abc" => NaN (Not a Number)
+// true => 1 ; false => 0
+
+let isLoggedIn=""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+
+let isLoggedIn1="Harsh"
+let booleanIsLoggedIn1 = Boolean(isLoggedIn1)
+
+console.log(booleanIsLoggedIn1);
+console.log(typeof booleanIsLoggedIn1);
+
+
+
+// "" => false 
+// "Harsh" => true
