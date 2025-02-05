146651dd5645d2be58176098220805a01b3a4fab
HARSH TIBREWAL <146108437+harshtibrewal02@users.noreply.github.com>
2025-02-05 10:06:22 +0000

    Javascript Execution Context

---
+++ 03_basics/07_jec.js
@@ -0,0 +1,27 @@
+// Javascript Execution  Context (JEC)
+
+// In browser the value of this is , " Window object".
+
+// Firstly program files has to "run" in  "Global EC" , which is allocated in "this".
+
+// Functional EC, Eval EC
+
+// firstly the files are sended to Memory "creation phase" ,where a space is allocated for the variables or anything in the program.
+
+// Secondly, then the are executed in the "Execution Phase".
+
+
+
+// Phase 1 : GEC => this
+// Phase 2 : Memory Creation :
+//         val1 = undefined
+//         val2 = undefined
+
+//         addNum = defintion(function)
+//         result1 = undefined
+//         result2 = undefined
+// Phase 3 : Execution Phase :
+//           Again 1. Memory Creation
+//           2. Execution Phase 
+
+// Then the result is reflected to the "GEC".
