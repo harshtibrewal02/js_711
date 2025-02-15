837b7dd7c9f0f4196e9663dd485b8f7a54c8e69c
HARSH TIBREWAL <146108437+harshtibrewal02@users.noreply.github.com>
2025-02-15 16:12:26 +0000

    Loopings Statement in JS

---
+++ 05_iterations/loops.js
@@ -0,0 +1,38 @@
+// while loop
+
+// let index = 0 ;
+
+// while (index <= 10) {
+//     console.log(`Value of index is : ${index}`);
+//     index = index + 2;
+    
+// }
+
+// let Myarray = ["harsh", "piyush", "dikshant"]
+//  let arr = 0;
+
+//  while (arr < Myarray.length) {
+//     console.log(`Value is ${Myarray[arr]}`);
+//     arr++;
+    
+//  }
+
+
+// do-while loop
+
+// in this loop :  first the work is done, then the conditons are checked
+
+
+// let score = 1
+
+// do {
+//     console.log(`Score is ${score}`);
+//     score++
+// } while (score <= 10);
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);  // Score is 11
